d0b4f1a370694f691d78b8b8eef02a9894c0d3b4
Bastian Inuk Christensen <bastian@inuk.blog>
2020-04-21 16:38:27 +0200

    Initial manga view

---
+++ src/components/MangaView.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+
+import { Manga } from "../models/Manga"
+
+export const MangaView = (manga: Manga) =>
+(
+    <div>
+
+    </div>
+)
---
+++ src/models/Manga.ts
@@ -0,0 +1,11 @@
+export class Manga
+{
+    title: string
+    cover: string
+    constructor (title: string, 
+                 cover: string)
+    {
+        this.title = title
+        this.cover = cover
+    }
+}
