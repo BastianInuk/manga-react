6e7aea3f20fa14f762a8f6fc7ece816cae55f570
Bastian Inuk Christensen <bastian@inuk.blog>
2020-04-23 21:38:21 +0200

    Initial lohin handler XHR

--- src/controllers/LoginHandler.ts
+++ src/controllers/LoginHandler.ts
@@ -1,4 +1,12 @@
 export const loginHandler = (username: string, password: string) =>
 {
     console.log(username)
+
+    let xhr = new XMLHttpRequest()
+    xhr.open('GET', "api.manga.dk/login")
+
+    xhr.onload = () => {
+
+    }
+
 }
