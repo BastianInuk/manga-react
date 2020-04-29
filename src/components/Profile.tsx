5deab535e524563179183b29759a0cd7e11cff03
Bastian Inuk Christensen <bastian@inuk.blog>
2020-04-29 09:57:49 +0200

    Added user protection in profile route

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 
 import { loginHandler } from "../controllers/LoginHandler"
+import { getCookie } from "../controllers/Cookies"
 
 let username: string = ""
 const setUsername = (usernameParam: string) => {
@@ -12,35 +13,69 @@ const setPassword = (passwordParam: string) => {
     password = passwordParam
 }
 
-const login = () =>
+const login = (event: React.FormEvent<HTMLFormElement>) =>
 {
+    console.log("Logging in")
     loginHandler(username, password)
+
+    // Prevents ugly stuff from happening in the url bar
+    event.preventDefault()
 }
+
+interface LoggedInProtection {
+    loggedIn: () => JSX.Element
+    notLoggedIn?: () => JSX.Element
+}
+
+const empty = () => (<div></div>)
+
+export const LoggedIn = (params: LoggedInProtection) => {
+    let user = getCookie("username")
+
+    let callback: () => JSX.Element    
+    if (user !== undefined)
+    {
+        callback = params.loggedIn
+    } else {
+        callback = params.notLoggedIn ?? empty
+    }
+    return callback()
+
+}
+
+const UserPanel = () => (
+    <div></div>
+)
+
+const LoginForm = () => 
+(
+    <form onSubmit={login}>
+        <label>
+            <p>Username:</p>
+            <input type="text" 
+                name="username" 
+                onChange={(e) => 
+                    setUsername(e.target.value)
+                } />
+        </label>
+        <br />
+        <label>
+            <p>Password:</p>
+            <input type="password" 
+                name="password" 
+                onChange={e => {
+                    console.log("Setting password")
+                    setPassword(e.target.value)
+                }} />
+        </label>
+        <br />
+        <input type="submit" value="Login" />
+    </form> 
+)
 
 export const ProfileBar = () =>
 (
     <nav className="ProfileBar">
-       <form onSubmit={login}>
-            <label>
-                <p>Username:</p>
-                <input type="text" 
-                       name="username" 
-                       onChange={(e) => 
-                           setUsername(e.target.value)
-                       } />
-            </label>
-            <br />
-            <label>
-                <p>Password:</p>
-                <input type="password" 
-                       name="password" 
-                       onChange={e => {
-                           console.log("Setting password")
-                           setPassword(e.target.value)
-                       }} />
-            </label>
-            <br />
-            <input type="submit" value="Login" />
-        </form> 
+        <LoggedIn notLoggedIn={LoginForm} loggedIn={UserPanel} />
     </nav>
 )
--- src/controllers/LoginHandler.ts
+++ src/controllers/LoginHandler.ts
@@ -1,12 +1,18 @@
+import { setCookie, getCookie, deleteCookie } from "./Cookies"
+
 export const loginHandler = (username: string, password: string) =>
 {
-    console.log(username)
+    setCookie("username", username)
 
     let xhr = new XMLHttpRequest()
     xhr.open('GET', "api.manga.dk/login")
-
+    xhr.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password))
     xhr.onload = () => {
 
     }
+
+    console.log(getCookie("username"))
+
+    deleteCookie("username")
 
 }
